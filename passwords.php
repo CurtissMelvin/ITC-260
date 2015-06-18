411832c31467c9fa2d021b5472d322eb2c9d1145
Curtiss Melvin <CurtissMelvin@users.noreply.github.com>
2015-06-18 13:27:10 -0700

    passphrase.php

--- passwords.php
+++ passwords.php
@@ -1,14 +1,49 @@
-<?php
-SESSION_start{};
-    $_SESSION ["password"] = "abc123";
-    if isset($_POST ["password"]){
-        //if what was entered has a value - is it equal to the session
-       if $_POST ["passsword"] == $_SESSION ["password"]{
-        //password is correct
-        echo "Correct Password"
-       } else{
-        //password is incorrect
-        echo "Wrong Password";
-       }
+
+    <?php
+        /*
+     *
+     * @package ITC260
+     * @author Curtiss <curtissm@hotmail.com>
+     * @version 1.0 2015/06/16
+     * @link http://www.curtissmelvin.com
+     * @license http://www.apache.org/licenses/LICENSE-2.0
+     * @Passphrase Assignment A3:
+     */
+
+
+    
+    if(session_status() == PHP_SESSION_NONE || session_id == "") {
+        session_start();
     }
-?>
+
+
+    if(!isset($_SESSION['login'])) {
+        $_SESSION['login'] = false;
+    }
+    
+     $pw = "abc123";
+    
+     if($_POST) {
+         IF($_POST['password'] == $pw) {
+             $_SESSION['login'] = true;
+         }else {
+             echo "Wrong password!";
+         }
+     }
+    
+     if(!$_SESSION["login"]) {
+         echo '
+         <!DOCTYPE html>
+         <html>
+         <body>
+            <form action="index.php" method="post">
+                <input type="text" name="password"><br />
+                <input type="submit" value="submit" />
+            </form>
+         </body>
+         </html>
+         ';
+         exit;
+     }
+    
+     ?>
